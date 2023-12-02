8a4d4f4f12cf74bb4199406bf506dbf078a7ecca
Adrianti Rusli <adriantirusli@gmail.com>
2023-12-02 10:36:45 +0700

    refactor: book list

--- src/books.ts
+++ src/books.ts
@@ -1,69 +1,98 @@
 export type Book = {
-  // Book ID (unique). It will also become the URL slug.
   id: number | string;
-  // Photo title
   title: string;
-  // Book URL (can be absolute or relative to the public folder)
   url: string;
-  // Book description
   description: string;
+  buy: string;
 };
 
 export const books: Book[] = [
   {
     id: 1,
-    title: 'Death by JavaScript',
+    title: 'The Mountain is You',
     description:
-      "Death by JavaScript: When you're so async, you forget to await your own life.",
-    url: '/images/books/the-happiness-project.webp',
+      'This is a book about self-sabotage. Why we do it, when we do it, and how to stop doing it—for good. Coexisting but conflicting needs create self-sabotaging behaviors. This is why we resist efforts to change, often until they feel completely futile. But by extracting crucial insight from our most damaging habits, building emotional intelligence by better understanding our brains and bodies, releasing past experiences at a cellular level, and learning to act as our highest potential future selves, we can step out of our own way and into our potential. For centuries, the mountain has been used as a metaphor for the big challenges we face, especially ones that seem impossible to overcome. To scale our mountains, we actually have to do the deep internal work of excavating trauma, building resilience, and adjusting how we show up for the climb. In the end, it is not the mountain we master, but ourselves.',
+    url: '/images/books/the-mountain-is-you.webp',
+    buy: 'https://tokopedia.link/Er6anpOUbFb',
   },
   {
     id: 2,
-    title: 'Death by JavaScript',
+    title: 'You Are A Badass At Making Money',
     description:
-      'Why did the developer die? They kept trying to destructure the meaning of life and got `undefined`.',
-    url: '/images/books/why-men-love-bitches.webp',
+      'You Are a Badass at Making Money will launch you past the fears and stumbling blocks that have kept financial success beyond your reach. Drawing on her own transformation—over just a few years—from a woman living in a converted garage with tumbleweeds blowing through her bank account to a woman who travels the world in style, Jen Sincero channels the inimitable sass and practicality that made You Are a Badass an indomitable bestseller. She combines hilarious personal essays with bite-size, aha concepts that unlock earning potential and get real results.',
+    url: '/images/books/you-are-a-badass-at-making-money.webp',
+    buy: 'https://tokopedia.link/rPuDXfNUbFb',
   },
   {
     id: 3,
-    title: 'Death by JavaScript',
+    title: 'The Psychology of Money',
     description:
-      'The developer passed away from trying to debug an obfuscated script in production.',
-    url: '/images/books/why-we-sleep.webp',
+      "Doing well with money isn't necessarily about what you know. It's about how you behave. And behavior is hard to teach, even to really smart people. Money--investing, personal finance, and business decisions--is typically taught as a math-based field, where data and formulas tell us exactly what to do. But in the real world people don't make financial decisions on a spreadsheet. They make them at the dinner table, or in a meeting room, where personal history, your own unique view of the world, ego, pride, marketing, and odd incentives are scrambled together. In The Psychology of Money, award-winning author Morgan Housel shares 19 short stories exploring the strange ways people think about money and teaches you how to make better sense of one of life's most important topics.",
+    url: '/images/books/psychology-of-money.webp',
+    buy: 'https://tokopedia.link/tdrCH0LUbFb',
   },
   {
     id: 4,
-    title: 'Death by JavaScript',
+    title: 'The Things You Can See Only When You Slow Down',
     description:
-      'The developer passed away from trying to debug an obfuscated script in production.',
+      'By offering guideposts to well-being and happiness in eight areas—including relationships, love, and spirituality—Haemin Sunim emphasizes the importance of forging a deeper connection with others and being compassionate and forgiving toward ourselves. The more than twenty full-color illustrations that accompany his teachings serve as calming visual interludes, encouraging us to notice that when you slow down, the world slows down with you.',
     url: '/images/books/the-things-you-can-see-only-when-you-slow-down.webp',
+    buy: 'https://tokopedia.link/28LKQdJUbFb',
   },
   {
     id: 5,
-    title: 'Death by JavaScript',
+    title: 'Kamu Gak Harus Sempurna',
     description:
-      "Death by JavaScript: When you're so async, you forget to await your own life.",
-    url: '/images/books/the-happiness-project.webp',
+      'Untuk kamu, yang memiliki keberanian untuk bertumbuh dan menyembukan diri dari pengalaman realasi yang menyakitkan dan "toxic". Untuk kamu, yang mulai ingin tahu bagaimana cara connect dengan diri yang dalam. Bagaimana cara mengenali, menerima, & menyadari emosi yang sedang dirasakan. Untuk semua orang yang memproses dan mengubah pengalaman pengkhianatan, kebencian, pengalaman disakiti, diabaikan, dan dibiarkan, menjadi ENERGI di dalam diri yang penh Welas Asih, penuh Cinta, dan Penuh Daya.',
+    url: '/images/books/kamu-gak-harus-sempurna.webp',
+    buy: 'https://shope.ee/3Ah41fGta3',
   },
   {
     id: 6,
-    title: 'Death by JavaScript',
+    title: 'Why We Sleep',
     description:
-      'Why did the developer die? They kept trying to destructure the meaning of life and got `undefined`.',
-    url: '/images/books/why-men-love-bitches.webp',
+      "Neuroscientist and sleep expert Matthew Walker provides a revolutionary exploration of sleep, examining how it affects every aspect of our physical and mental well-being. Charting the most cutting-edge scientific breakthroughs, and marshalling his decades of research and clinical practice, Walker explains how we can harness sleep to improve learning, mood and energy levels, regulate hormones, prevent cancer, Alzheimer's and diabetes, slow the effects of aging, and increase longevity. He also provides actionable steps towards getting a better night's sleep every night.",
+    url: '/images/books/why-we-sleep.webp',
+    buy: 'https://tokopedia.link/nEzH5gSUbFb',
   },
   {
     id: 7,
-    title: 'Death by JavaScript',
+    title: 'Adult Children of Emotionally Immature Parents',
     description:
-      'The developer passed away from trying to debug an obfuscated script in production.',
-    url: '/images/books/why-we-sleep.webp',
+      'In this breakthrough book, clinical psychologist Lindsay Gibson exposes the destructive nature of parents who are emotionally immature or unavailable. You will see how these parents create a sense of neglect, and discover ways to heal from the pain and confusion caused by your childhood. By freeing yourself from your parents’ emotional immaturity, you can recover your true nature, control how you react to them, and avoid disappointment. Finally, you’ll learn how to create positive, new relationships so you can build a better life.',
+    url: '/images/books/adult-children-of-emotionally-immamture-parents.webp',
+    buy: 'https://shope.ee/7UqDmAYSBQ',
   },
   {
     id: 8,
-    title: 'Death by JavaScript',
+    title: "The Alpha Girl's Guide",
     description:
-      'The developer passed away from trying to debug an obfuscated script in production.',
-    url: '/images/books/the-things-you-can-see-only-when-you-slow-down.webp',
+      'Alpha Female adalah para perempuan yang menginspirasi, memimpin, menggerakkan orang sekitarnya, dan membawa perubahan. Mereka cerdas, percaya diri, dan independen. Buku ini adalah hasil pengamatan, riset artikel, wawancara langsung, dan diskusi dengan banyak perempuan di media sosial Ask.fm. Ditulis dengan ringan, penuh ilustrasi kocak, tetapi tetap blak-blakan menohok, The Alpha Girl’s Guide akan membuat kamu terinspirasi menjadi cewek smart, independen dan bebas galau!',
+    url: '/images/books/the-alpha-girls-guide.webp',
+    buy: 'https://shope.ee/4ppSbIUsCn',
+  },
+  {
+    id: 9,
+    title: 'Nggak Apa Apa Kalau Sedih Nangis Aja',
+    description:
+      'Dalam buku ini, kita akan diajak untuk connect dengan inner child kita. Apa sih inner child itu? Inner child adalah sifat atau perilaku kekanakan yang ada dalam diri kita. Setiap dari kita pasti punya inner child dalam diri kita. Tapi, mungkin ada yang menyadarinya, tidak menyadarinya, ataupun menampiknya. Oleh karena itu, buku ini disajikan agar kita bisa menerima dan mengakui inner child dan setiap emosi dengan diri kita. Dengan menerima dan mengakuinya, kita pun bisa berdamai dengan inner child dan semua emosi tersebut.',
+    url: '/images/books/nggak-apa-apa-kalau-sedih-nangis-aja.webp',
+    buy: 'https://shope.ee/3Ah41fGta3',
+  },
+  {
+    id: 10,
+    title: 'Why Men Love Bitches',
+    description:
+      "Do you feel like you are too nice? Sherry Argov's Why Men Love Bitches delivers a unique perspective as to why men are attracted to a strong woman who stands up for herself. With saucy detail on every page, this no-nonsense guide reveals why a strong woman is much more desirable than a `yes woman` who routinely sacrifices herself.",
+    url: '/images/books/why-men-love-bitches.webp',
+    buy: 'https://tokopedia.link/ZlafUo5UbFb',
+  },
+  {
+    id: 11,
+    title: 'The Happiness Project',
+    description:
+      'Gretchen Rubin had an epiphany one rainy afternoon in the unlikeliest of places: a city bus. “The days are long, but the years are short,” she realized. “Time is passing, and I’m not focusing enough on the things that really matter.” In that moment, she decided to dedicate a year to her happiness project.',
+    url: '/images/books/the-happiness-project.webp',
+    buy: 'https://tokopedia.link/sYSU3f4UbFb',
   },
 ];
